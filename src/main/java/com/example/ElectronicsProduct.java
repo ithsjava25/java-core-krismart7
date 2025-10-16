5ce1e3fb40b251a230311193fb563afb50462072
Kristina M <kristinamartinovic777@gmail.com>
2025-10-16 17:28:26 +0200

    Refactor shipping cost calculation to use a threshold constant for clarity and maintainability

--- src/main/java/com/example/ElectronicsProduct.java
+++ src/main/java/com/example/ElectronicsProduct.java
@@ -8,6 +8,10 @@ public class ElectronicsProduct extends Product implements Shippable {
     private final int warrantyMonths;
     private final BigDecimal weight;
 
+    private static final BigDecimal BASE_SHIPPING = BigDecimal.valueOf(79);
+    private static final BigDecimal ADD_SHIPPING = BigDecimal.valueOf(49);
+    private static final BigDecimal WEIGHT_THRESHOLD = BigDecimal.valueOf(5.0);
+
     public ElectronicsProduct(UUID id, String name, Category category, BigDecimal price, int warrantyMonths, BigDecimal weight) {
         super(id, name, category, price);
 
@@ -26,9 +30,9 @@ public class ElectronicsProduct extends Product implements Shippable {
 
     @Override
     public BigDecimal calculateShippingCost() {
-        BigDecimal cost = BigDecimal.valueOf(79);
-        if (weight.compareTo(BigDecimal.valueOf(5.0)) > 0) {
-            cost = cost.add(BigDecimal.valueOf(49));
+        BigDecimal cost = BASE_SHIPPING;
+        if (weight.compareTo(WEIGHT_THRESHOLD) > 0) {
+            cost = cost.add(ADD_SHIPPING);
         }
         return cost.setScale(2, RoundingMode.HALF_UP);
     }
